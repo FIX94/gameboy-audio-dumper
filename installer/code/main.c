3d32b2e26bb5f8a358756c0e1017d8613fdba9eb
FIX94 <fix94.1@gmail.com>
2018-10-24 23:20:15 +0200

    added "updater" (at the moment only for the german version) which allows you to install new code onto a cart much quicker when the installer was used previously without having to reset the save again to install changes, it will just overwrite whatever was installed previously

--- installer/code/main.c
+++ installer/code/main.c
@@ -14,10 +14,12 @@ typedef struct _copyinfo {
 	size_t offset;
 	size_t length;
 } copyinfo;
-static copyinfo info[2][COPYTOTAL] =
-{
+static copyinfo info[3][COPYTOTAL] =
+{//installer
   { { 53454, 0x381, 0xE1 }, { 54943, 0x281, 0x100 }, { 55872, 0x181, 0x100 }, { 56801, 0x81, 0x100 }, { 57730, 0, 0x81 } }, //pokemon yellow
   { { 53842, 0x37C, 0xE6 }, { 55282, 0x27C, 0x100 }, { 56220, 0x17C, 0x100 }, { 57158, 0x7C, 0x100 }, { 58096, 0, 0x7C } }, //pokemon gelb
+//updater
+  { {  7176, 0x37C, 0xE6 }, {  8625, 0x27C, 0x100 }, {  9563, 0x17C, 0x100 }, { 10501, 0x7C, 0x100 }, { 11439, 0, 0x7C } }, //pokemon gelb
 };
 
 int main()
@@ -42,6 +44,11 @@ int main()
 	else if(fsize == 803458)
 	{
 		cinfo = info[1];
+		csendername = "../sender/gelb.bin";
+	}
+	else if(fsize == 167004)
+	{
+		cinfo = info[2];
 		csendername = "../sender/gelb.bin";
 	}
 	else
--- installer/make_pokemon_gelb_bk2.bat
+++ installer/make_pokemon_gelb_bk2.bat
@@ -1,12 +1,27 @@
 md tmp
-copy "pokemon_gelb\Comments.txt" "tmp\Comments.txt"
-copy "pokemon_gelb\Header.txt" "tmp\Header.txt"
-copy "pokemon_gelb\Input Log.txt" "tmp\Input Log.txt"
-copy "pokemon_gelb\Subtitles.txt" "tmp\Subtitles.txt"
-copy "pokemon_gelb\SyncSettings.json" "tmp\SyncSettings.json"
+copy "pokemon_gelb_installer\Comments.txt" "tmp\Comments.txt"
+copy "pokemon_gelb_installer\Header.txt" "tmp\Header.txt"
+copy "pokemon_gelb_installer\Input Log.txt" "tmp\Input Log.txt"
+copy "pokemon_gelb_installer\Subtitles.txt" "tmp\Subtitles.txt"
+copy "pokemon_gelb_installer\SyncSettings.json" "tmp\SyncSettings.json"
 
 "code\inst"
 "zip\zip" -JXjq9 fix94-pokemongelb-installer.bk2 "tmp\Comments.txt" "tmp\Header.txt" "tmp\Input Log.txt" "tmp\Subtitles.txt" "tmp\SyncSettings.json"
+
+del "tmp\Comments.txt"
+del "tmp\Header.txt"
+del "tmp\Input Log.txt"
+del "tmp\Subtitles.txt"
+del "tmp\SyncSettings.json"
+
+copy "pokemon_gelb_updater\Comments.txt" "tmp\Comments.txt"
+copy "pokemon_gelb_updater\Header.txt" "tmp\Header.txt"
+copy "pokemon_gelb_updater\Input Log.txt" "tmp\Input Log.txt"
+copy "pokemon_gelb_updater\Subtitles.txt" "tmp\Subtitles.txt"
+copy "pokemon_gelb_updater\SyncSettings.json" "tmp\SyncSettings.json"
+
+"code\inst"
+"zip\zip" -JXjq9 fix94-pokemongelb-updater.bk2 "tmp\Comments.txt" "tmp\Header.txt" "tmp\Input Log.txt" "tmp\Subtitles.txt" "tmp\SyncSettings.json"
 
 del "tmp\Comments.txt"
 del "tmp\Header.txt"
